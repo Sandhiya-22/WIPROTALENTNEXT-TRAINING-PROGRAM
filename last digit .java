2850833ef3a969ca1511fe3e6db08b6a2b570a70
Sandhiya-22 <91065235+Sandhiya-22@users.noreply.github.com>
2023-02-08 19:09:19 +0530

    Add files via upload

---
+++ last digit .java
@@ -0,0 +1,17 @@
+Return the last digit of the given number.
+
+
+
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+ public int lastDigitOf(int input1){
+     if(input1<0)
+  {
+   input1*=-1;
+  }
+  int n=input1%10;
+  return n;
+ }
+}
